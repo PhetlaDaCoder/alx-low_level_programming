8894f9d4aed819a2b1a5167e237242a79f592fa5
PhetlaDaCoder <hsphetla@gmail.com>
2024-06-20 12:16:52 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /** main - printsout text
  *
  * Return: 0 When done
- */
+*/
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("wiith proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
