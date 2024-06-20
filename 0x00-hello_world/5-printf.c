1bf76df07aa348371203aa90d3350085c1f07442
PhetlaDaCoder <hsphetla@gmail.com>
2024-06-20 12:12:11 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/** main - printsout text
+ *
+ * Return: 0 When done
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
