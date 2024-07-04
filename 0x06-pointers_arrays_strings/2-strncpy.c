6271d98bf62f8214a2ba8e7aa79896f107575c21
PhetlaDaCoder <hsphetla@gmail.com>
2024-07-04 06:21:19 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,13 +13,19 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && *(src + i); i++)
+	i =0;
+
+	while (src[i] != '\0' && i < n)
 	{
-		*(dest + i) = *(src + i);
+		dest[i] = src[i];
+		i++;
 	}
-	for (; i < n; i++)
+
+	while (i < n)
 	{
-	*(dest + i) = '\0';
+		dest[i] = '\0';
+		i++;
 	}
+
 	return (dest);
 }
