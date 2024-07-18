dfa8d56dcd84400079f168d22eafb4c03ef14ab9
PhetlaDaCoder <hsphetla@gmail.com>
2024-07-18 08:23:21 -0700

    3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * array_range - creates an array of integers
+ * @min: smallest number
+ * @max: biggest number
+ *
+ * Return: address pointer
+ */
+
+int *array_range(int min, int max)
+{
+	int *block;
+	int i, j = 0;
+
+	if (min > max)
+		return (NULL);
+	block = malloc(sizeof(*block) * ((max - min) + 1));
+	if (block != NULL)
+	{
+		for (i = min; i <= max; i++)
+		{
+			block[j] = i;
+			j++;
+		}
+		return (block);
+	}
+	else
+		return (NULL);
+}
