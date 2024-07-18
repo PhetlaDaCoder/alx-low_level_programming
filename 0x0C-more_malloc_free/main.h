aa344260acde495a66206bf38dcbe7a457191610
PhetlaDaCoder <hsphetla@gmail.com>
2024-07-18 12:36:35 -0700

    101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -17,6 +17,9 @@ char *create_xarray(int size);
 char *iterate_zeroes(char *str);
 void get_prod(char *prod, char *mult, int digits, int zeroes);
 void add_nums(char *final_prod, char *next_prod, int next_len);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 
 
 #endif
