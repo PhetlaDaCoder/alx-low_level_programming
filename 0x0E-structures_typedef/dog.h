15763383cd1831abfa8ad59247adc7966d2a09bd
PhetlaDaCoder <hsphetla@gmail.com>
2024-07-22 13:22:47 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -26,5 +26,6 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpt(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
