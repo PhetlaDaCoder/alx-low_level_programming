f1aeafaddffc81b9f7e5bce9892b8508843d0015
PhetlaDaCoder <hsphetla@gmail.com>
2024-07-24 08:34:58 -0700

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,6 +1,5 @@
-#include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include "3-calc.h"
 
 /**
  * main - main function
@@ -12,38 +11,22 @@
 int main(int argc, char *argv[])
 {
 
-	int arg1, arg2, result;
-	char o;
-	int (*func)(int, int);
+        int (*oprt)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+        if (argc != 4)
+        {
+                printf("Error\n");
+                exit(98);
+        }
 
-	arg1 = atoi(argv[1]);
-	arg2 = atoi(argv[3]);
+        oprt = get_op_func(argv[2]);
 
-	func = get_op_func(argv[2]);
+        if (!oprt)
+        {
+                printf("Error\n");
+                exit(99);
+        }
 
-	if (!func)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	o = *arhv[2];
-
-	if ((o == '/' || 0 == '%') && arg2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	result = func(arg1, arg2);
-
-	printf("%d\n", result);
-
-	return (0);
+        printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+        return (0);
 }
