d01d7852e5fe9e8054c4f8ddd83d0c901bfa5369
PhetlaDaCoder <hsphetla@gmail.com>
2024-07-26 04:53:35 -0700

    fg

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,13 +22,12 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	if (i < 5)
-		return (0);
-
-		if (strcmp(s, ops) == 0)
+	if (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
-
 		i++;
+	}
 
-		return (0);
+	return (NULL);
 }
