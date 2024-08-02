71284954fb6e819643817873281b89635f91193f
PhetlaDacoder <hsphetla@gmail.com>
2024-08-01 18:06:54 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * prints_list - A function that prints singly linked list
+ * @h: singly lonked list head pointer
+ *
+ * Return: value of list elements
+ */
+
+size_t print_list(const list_t *h);
+{
+	unsigned int size = 0;
+
+	while (h != NULL)
+	{
+		if (o->str != 0)
+			printf("[%i] %\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
+		size++;
+	}
+	return (size);
+}
