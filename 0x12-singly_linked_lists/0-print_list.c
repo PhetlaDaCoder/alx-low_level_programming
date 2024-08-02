7d471d0faf768cf5055b5653a81b1d13cf8cd5e4
PhetlaDacoder <hsphetla@gmail.com>
2024-08-01 18:46:30 -0700

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,26 +3,21 @@
 #include <stdlib.h>
 
 /**
- * prints_list - A function that prints singly linked list
+ * print_list - A function that prints singly linked list
  * @h: singly lonked list head pointer
  *
  * Return: value of list elements
  */
-
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
 {
 	unsigned int size = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
-
-		}	printf("[%d] %\n", 0, "(nil)");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
 		h = h->next;
 		size++;
 	}
