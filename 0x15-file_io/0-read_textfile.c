891c38a4c2cf3af46215446c5eeaa2c0481b26d6
PhetlaDacoder <hsphetla@gmail.com>
2024-08-12 07:36:54 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,37 +11,28 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int op;
-	int r, w;
-	char *buffer;
+	ssize_t o, r, w;
+	char *buf;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
-	op = open(filename, O_RDONLY);
-	if (op < 0)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
-		return (0);
-	r = read(op, buffer, letters);
-	if (r < 0)
+
+	o = open(filename, o_RDONLY);
+	r = read(0, buf, letters);
+	w = write(STDOUT_FILENO, buf, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
-		free(buffer);
+		free(buf);
 		return (0);
 	}
-	buffer[r] = '\0';
-	close(fd);
 
-	w = write(STDOUT_FILENO, buffer, r);
-	if (w < 0)
-	{
-		free(buffer);
-		return (0);
-	}
-	free(*buffer);
+	free(buf);
+	close(o);
+
 	return (w);
 }
-
-
-
