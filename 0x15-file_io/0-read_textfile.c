87bdb86c6a53c8b1cd76d57a130783f88c1a72f0
PhetlaDacoder <hsphetla@gmail.com>
2024-08-12 09:16:27 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	o = open(filename, O_RDONLY);
-	r = read(0, buf, letters);
+	r = read(o, buf, letters);
 	w = write(STDOUT_FILENO, buf, r);
 
 	if (o == -1 || r == -1 || w == -1 || w != r)
